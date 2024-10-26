fe968489a99a701113a4f35e464e7861ff38123c
Maxim Tarasov <31613333+TarasovMV@users.noreply.github.com>
2024-10-26 16:44:10 +0300

    chore: clear cookie

--- parsers/parse_data.py
+++ parsers/parse_data.py
@@ -8,7 +8,7 @@ menu_url = 'https://twork.tinkoff.ru/knowledge-base/api/sections/list?spaceCode=
 sections_url = 'https://twork.tinkoff.ru/knowledge-base/api/subsections'
 article_url = 'https://twork.tinkoff.ru/knowledge-base/api/articles'
 # Куки в виде строки
-cookie_str = 'Pwuid=1a322b8faf9f4510a1532f994f7d71e1; stDeIdU=1a322b8faf9f4510a1532f994f7d71e1; sso_used=true; idsrv.uid=9c5d1ce1-3923-4790-9a06-6d44a97ba920; dsp_click_id=no%20dsp_click_id; _ym_uid=1723619321745406654; _ym_d=1723619321; dco.id=7399f3e2-06fb-4052-a8a7-0000115cb11e; userType=Visitor; amp_5a7f16=7a-sIB-4rFLI0c3IUJYECM...1i5s8ve1g.1i5s8ve1g.0.0.0; _ga=GA1.2.464451416.1723791511; _ga_EB19KZQJXZ=GS1.1.1724304570.4.1.1724304812.60.0.0; _ga_43H68Z69W3=GS1.1.1724304570.4.1.1724304812.60.0.0; utm_source=search; utm_date_set=1727684684986; Pwuid_last_update_time=1729232837793; Pwuid_visit_id=v1%3A0000125%3A1729266715883%3A1a322b8faf9f4510a1532f994f7d71e1; idsrv.login-id=vF0vS3DBkCn9oTgf7NLi; idsrv.time=1729355634; idsrv=CfDJ8CQxqrVrNhRIoA_pG3fqKDyU7lpCVGDQy1v9AK8fCInyX_ECwJp7J2GBbzmTuSNRJYyxSyPmc2Jr2rAWCR_ovNJnkU2diUYNYXeqkP1eGJoqmbN5OhnVG0ypjFmiTMjuOCIRp_l6M_azEVhuodR5sg3Kecxk7tfC7IU9NW6Sc2sIlPx4xz04Bcl1xSIin5asOi5uNaywWbwKVGre5DHvIUVibTJXk7jyAh888DE45ESdoPywbRYR7bKv45UOXYSmxtvWsQEB4442VA4vHCckN3w; _twork_oauth2_proxy=djIuWDNSM2IzSnJYMjloZFhSb01sOXdjbTk0ZVMwMVltTXpZekJtT0RoaFpEWm1aalptWkRWaU5HSTVZakJtTmpkaFpETXdNUS56eVhFckZKT2g3TzMyczNLYU0zaldR|1729355634|O80BBXyjgHiD7XZuf5ixHh7fVbrRj6zRFAIDrOm3gX0=; vIdUid=19dcccf9-9ee6-4227-9789-84531bacfda7; stSeStTi=1729355635274; stLaEvTi=1729355764493'
+cookie_str = ''
 # Элементы меню
 valid_menu_items = ['SME. Онлайн-бухгалтерия', 'SME. Бухгалтерское обслуживание']
 # Заголовки, в которые передаем cookie
